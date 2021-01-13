7d9f8cbf19eacb3289b428743dd7764e60225346
Abdullah-rahim <gthe4538@gmail.com>
2021-01-13 17:39:20 +0500

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Weatherapp from './Weather.jsx'
+
+function App() {
+  return (
+    <div>
+      <Weatherapp/>
+    </div>
+  )
+}
+
+export default App;
